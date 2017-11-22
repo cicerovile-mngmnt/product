fe9e391c4e28b37a72025532fe2c150819866fb9
michaelcouck <michael.couck@gmail.com>
2017-11-22 11:06:23 +0100

    Changed the server port.

--- src/main/java/com/fuck/off/fuckoffasaservice/FuckOffAsAServiceApplication.java
+++ src/main/java/com/fuck/off/fuckoffasaservice/FuckOffAsAServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.context.web.SpringBootServletInitializer;
 @SpringBootApplication
 public class FuckOffAsAServiceApplication extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         SpringApplication.run(FuckOffAsAServiceApplication.class, args);
     }
 
